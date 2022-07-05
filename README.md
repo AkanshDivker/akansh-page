4cd40049d131ed71bf35255f848083e4362d8ba5
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-05 20:54:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# akansh-page
+ 
---
+++ components/ModeToggle.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useColorScheme } from "@mui/joy/styles";
+import Button from "@mui/joy/Button";
+
+const ModeToggle = () => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  // necessary for server-side rendering
+  // because mode is undefined on the server
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        if (mode === "light") {
+          setMode("dark");
+        } else {
+          setMode("light");
+        }
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+};
+
+export default ModeToggle;
