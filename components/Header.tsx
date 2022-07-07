783c868bff543a4c1d3919620203613f163ae9da
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-07 14:39:24 +0900

    Refactor style

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,20 +4,22 @@ import type { NextPage } from "next";
 import { header } from "../resources/data";
 import Navbar from "./Navbar";
 
-const headerStyle = {
-  height: "8em",
-  display: "flex",
-  margin: "0 auto",
-  marginTop: "25px",
-  width: "70%",
-  justifyContent: "space-between",
+const style = {
+  header: {
+    height: "8em",
+    display: "flex",
+    margin: "0 auto",
+    marginTop: "25px",
+    width: "70%",
+    justifyContent: "space-between",
+  },
 };
 
 const Header: NextPage = () => {
   const { title, url } = header;
 
   return (
-    <header style={headerStyle}>
+    <header style={style.header}>
       <Typography level="h3">
         <Link
           color="primary"
