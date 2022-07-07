fb228ecc1e3df3cff27965992c1c0d45177ce8e6
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-07 10:30:28 +0900

    Add Header
    Add Header component to main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,11 @@
-import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
-import ModeToggle from "../components/ModeToggle";
+import { CssVarsProvider } from "@mui/joy/styles";
 import type { NextPage } from "next";
+import Header from "../components/Header";
 
 const Index: NextPage = () => {
   return (
     <CssVarsProvider>
-      <ModeToggle />
-      <Sheet
-        sx={{
-          maxWidth: 400,
-          mx: 'auto', // margin left & right
-          my: 4, // margin top & botom
-          py: 3, // padding top & bottom
-          px: 2, // padding left & right
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          borderRadius: 'sm',
-          boxShadow: 'md',
-        }}
-      >
-        <div>
-          <Typography level="h4" component="h1">
-            <b>Welcome!</b>
-          </Typography>
-          <Typography level="body2">Sign in to continue</Typography>
-        </div>
-    </Sheet>
+      <Header />
     </CssVarsProvider>
   );
 };
