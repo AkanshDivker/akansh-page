71054874934fee15251634f0f43020557b8a8fcb
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-07 14:05:54 +0900

    Added theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { CssVarsProvider } from "@mui/joy/styles";
 import type { NextPage } from "next";
 import Header from "../components/Header";
+import theme from "../components/Theme";
 
 const Index: NextPage = () => {
   return (
-    <CssVarsProvider>
+    <CssVarsProvider theme={theme}>
       <Header />
     </CssVarsProvider>
   );
