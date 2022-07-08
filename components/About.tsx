b0b9e87de7ca97dc59f5a6d7ac5bfb7cfbc81260
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-08 12:03:56 +0900

    Update styling and content
    Conform to Joy UI styling conventions and update section content.

--- components/About.tsx
+++ components/About.tsx
@@ -1,26 +1,86 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Box from "@mui/joy/Box";
+import IconButton from "@mui/joy/IconButton";
 import Typography from "@mui/joy/Typography";
 import type { NextPage } from "next";
 
-const style = {
-  about: {
-    display: "flex",
-    alignItems: "center",
-    alignSelf: "center",
-    flexDirection: "column" as "column", // Error fix
-    marginTop: "3em",
-  },
-};
-
 const About: NextPage = () => {
   return (
-    <div style={style.about}>
-      <Typography level="h1">
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        alignSelf: "center",
+        flexDirection: "column",
+        marginTop: 3,
+      }}
+    >
+      <Typography level="display1">
         {"Hi, I'm "}
-        <Typography level="h1" color="primary" textColor="primary">
+        <Typography level="display1" color="primary" textColor="primary">
           Akansh.
         </Typography>
       </Typography>
-    </div>
+
+      <Typography level="h3" fontWeight="700" sx={{ marginTop: 3 }}>
+        A Security Engineer.
+      </Typography>
+
+      <Typography
+        level="body1"
+        sx={{
+          alignItems: "flex-start",
+          maxWidth: 500,
+          wordBreak: "break-word",
+          marginTop: 3,
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        vehicula arcu id nulla blandit, at luctus erat dictum. Phasellus dapibus
+        sapien ut lacus congue, ac hendrerit diam aliquam. Sed molestie dictum
+        neque eu molestie. Nulla rutrum ultricies nisi non tincidunt.
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          flexWrap: "wrap",
+          alignItems: "center",
+          marginTop: 3,
+        }}
+      >
+        <IconButton
+          component="a"
+          variant="plain"
+          target="_blank"
+          rel="noopener"
+          href="/resume.pdf"
+        >
+          <InsertDriveFileIcon />
+        </IconButton>
+        <IconButton
+          component="a"
+          variant="plain"
+          target="_blank"
+          rel="noopener"
+          href="https://www.linkedin.com/in/akanshdivker/"
+        >
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton
+          component="a"
+          variant="plain"
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/AkanshDivker"
+        >
+          <GitHubIcon />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
