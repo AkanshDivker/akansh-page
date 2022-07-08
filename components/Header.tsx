0b11f0020bcdad76c67afb42b430ebf153d675c8
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2022-07-08 11:11:00 +0900

    Conform to Joy styling
    Conform to Joy UI conventions for styling components.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,38 @@
+import Box from "@mui/joy/Box";
 import Link from "@mui/joy/Link";
 import Typography from "@mui/joy/Typography";
 import type { NextPage } from "next";
 import { header } from "../resources/data";
 import Navbar from "./Navbar";
 
-const style = {
-  header: {
-    height: "8em",
-    display: "flex",
-    margin: "0 auto",
-    marginTop: "25px",
-    width: "70%",
-    justifyContent: "space-between",
-  },
-};
-
 const Header: NextPage = () => {
   const { title, url } = header;
 
   return (
-    <header style={style.header}>
-      <Typography level="h3">
-        <Link
-          color="primary"
-          disabled={false}
-          level="h3"
-          underline="none"
-          variant="plain"
-          href={url}
-        >
-          {title}
-        </Link>
-      </Typography>
-      <Navbar />
+    <header>
+      <Box
+        sx={{
+          height: "8em",
+          display: "flex",
+          margin: "0 auto",
+          marginTop: "25px",
+          width: "70%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography level="h3">
+          <Link
+            color="primary"
+            level="h3"
+            underline="none"
+            variant="plain"
+            href={url}
+          >
+            {title}
+          </Link>
+        </Typography>
+        <Navbar />
+      </Box>
     </header>
   );
 };
