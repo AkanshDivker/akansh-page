5b8f8aa6719b1a566ad29eb623e36563a70db163
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2024-06-25 18:18:19 +0900

    Convert to functional

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,37 @@
 import { getInitColorSchemeScript } from "@mui/joy/styles";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html data-color-scheme="light">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7d67a4" />
-          <meta name="msapplication-TileColor" content="#603cba" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
-        <body>
-          {getInitColorSchemeScript({ defaultMode: "light" })}
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html data-color-scheme="light" lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7d67a4" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <body>
+        {getInitColorSchemeScript({ defaultMode: "light" })}
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
