ba20993fc707aff3a338252ce29c1551847313a4
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2024-06-25 18:43:55 +0900

    Remove tools

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,84 +1,11 @@
-import ClickAwayListener from "@mui/base/ClickAwayListener";
-import PopperUnstyled from "@mui/base/PopperUnstyled";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import Box from "@mui/joy/Box";
 import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
 import ListItemButton from "@mui/joy/ListItemButton";
-import ListItemContent from "@mui/joy/ListItemContent";
-import Sheet from "@mui/joy/Sheet";
 import Typography from "@mui/joy/Typography";
 import type { NextPage } from "next";
 import React from "react";
 import ModeToggle from "./ModeToggle";
-
-const ToolsMenu: NextPage = () => {
-  const [anchorEl, setAnchorEl] = React.useState<
-    EventTarget & HTMLAnchorElement
-  >();
-  const open = Boolean(anchorEl);
-
-  const onFocus = (event: React.FocusEvent<HTMLAnchorElement, Element>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const onMouseEnter = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-  ) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  return (
-    <ClickAwayListener onClickAway={() => setAnchorEl(undefined)}>
-      <Box onMouseLeave={() => setAnchorEl(undefined)}>
-        <ListItemButton
-          aria-haspopup
-          aria-expanded={open ? "true" : "false"}
-          role="menuitem"
-          onFocus={onFocus}
-          onMouseEnter={onMouseEnter}
-          sx={(theme) => ({
-            ...(open && theme.variants.plainHover.neutral),
-          })}
-        >
-          <Typography level="title-md">Tools</Typography>
-          <KeyboardArrowDown />
-        </ListItemButton>
-        <PopperUnstyled
-          open={open}
-          anchorEl={anchorEl}
-          disablePortal
-          keepMounted
-        >
-          <Sheet
-            variant="outlined"
-            sx={{ my: 2, boxShadow: "md", borderRadius: "sm", minWidth: 180 }}
-          >
-            <List
-              role="menu"
-              aria-label="Tools"
-              sx={{
-                "--List-radius": "8px",
-                "--List-padding": "4px",
-              }}
-            >
-              <ListItem role="none">
-                <ListItemButton role="menuitem" href="#" disabled>
-                  <ListItemContent>ATLAS</ListItemContent>
-                </ListItemButton>
-              </ListItem>
-              <ListItem role="none">
-                <ListItemButton role="menuitem" href="#" disabled>
-                  PolyEngine
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Sheet>
-        </PopperUnstyled>
-      </Box>
-    </ClickAwayListener>
-  );
-};
 
 const Navbar: NextPage = () => {
   return (
@@ -108,9 +35,6 @@ const Navbar: NextPage = () => {
           <ListItemButton role="menuitem" component="a" href="#projects">
             <Typography level="title-md">Projects</Typography>
           </ListItemButton>
-        </ListItem>
-        <ListItem role="none">
-          <ToolsMenu />
         </ListItem>
         <ListItem role="none">
           <ModeToggle />
