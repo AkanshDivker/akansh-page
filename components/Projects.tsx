7cea19715b9df0b74cc842ce749330929d135ca3
Akansh <7958962+AkanshDivker@users.noreply.github.com>
2024-06-25 18:44:01 +0900

    Update projects list

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -52,22 +52,6 @@ const Projects: NextPage = () => {
         }}
       >
         <ProjectCard
-          title="ATLAS"
-          url="https://atlas.akansh.xyz"
-          description="An automated system for analyzing binary files and determining common threat parameters, such as the malware family and potentially malicious URLs."
-          techList={["Python", "TypeScript", "Next.js", "MongoDB"]}
-          chipColor="primary"
-        />
-
-        <ProjectCard
-          title="Identity"
-          url="#"
-          description="A SaaS prototype app to provide a solution to secure and universal passwordless authentication on the web."
-          techList={["JavaScript", "Node.js", "React.js", "Kubernetes"]}
-          chipColor="primary"
-        />
-
-        <ProjectCard
           title="PolyEngine"
           url="https://github.com/AkanshDivker/polyengine"
           description="A Python application to apply code encryption, obfuscation, and polymorphism on compile time for C/C++ programs."
@@ -84,13 +68,21 @@ const Projects: NextPage = () => {
         />
 
         <ProjectCard
+          title="GitClean"
+          url="https://github.com/AkanshDivker/GitClean"
+          description="A simple Python script to clean files from directories based on their extensions."
+          techList={["Python"]}
+          chipColor="primary"
+        />
+
+        <ProjectCard
           title="Simple-ECS"
           url="https://github.com/AkanshDivker/Simple-ECS"
           description="An example using the core features of the Entity Component System (ECS) and the Data Oriented Tech Stack (DOTS) in Unity, inspired by Roll-a-ball."
           techList={["C#", "Unity", "DOTS", "ECS"]}
           chipColor="primary"
         />
-        
+
         <ProjectCard
           title="Akansh-Page"
           url="https://github.com/AkanshDivker/akansh-page"
